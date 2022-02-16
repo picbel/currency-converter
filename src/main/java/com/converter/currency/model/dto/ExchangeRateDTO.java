829277f98f47d194bf4e499b7de9058d574b900f
Picbel <picbel.gon@gmail.com>
2022-02-17 00:10:37 +0900

    Initial commit

---
+++ src/main/java/com/converter/currency/model/dto/ExchangeRateDTO.java
@@ -0,0 +1,15 @@
+package com.converter.currency.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Setter
+@Getter
+public class ExchangeRateDTO {
+
+
+    String aa;
+    Integer a;
+}
